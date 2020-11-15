4d1ea5e409d07c8e65dc11004c18b7a659be314e
Netanel Tamir <tamir.netanel@gmail.com>
2020-11-15 15:48:34 +0200

    Added ChessGameParser.py

---
+++ ChessGameParser.py
@@ -0,0 +1,22 @@
+import chess
+import numpy as np
+
+arr=np.zeros((8,8))
+
+#White:
+#Pawn: 1
+#Knight: 2
+#Bishop: 3
+#Rook: 4
+#Queen: 5
+#King: 6
+
+#Black:
+#Pawn: 11
+#Knight: 12
+#Bishop: 13
+#Rook: 14
+#Queen: 15
+#King: 16
+
+print(arr)
