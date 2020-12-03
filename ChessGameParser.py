d5535b773eee6e08ad8e4fb7fecff8b161193321
Netanel Tamir <tamir.netanel@gmail.com>
2020-12-03 22:07:00 +0200

    3/12

--- ChessGameParser.py
+++ ChessGameParser.py
@@ -1,11 +1,14 @@
 import chess.pgn
 import numpy as np
+from matplotlib import transforms
 from torch import nn, optim
 from torch.utils import data
 import torch
 import matplotlib.pyplot as plt
+from chess import engine
 
 engine = chess.engine.SimpleEngine.popen_uci("stockfish")
+
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
@@ -13,26 +16,19 @@ class NeuralNetwork(nn.Module):
         Declare layers for the model
         """
         super().__init__()
-        self.fc1 = nn.Linear(1536, 256)
-        self.fc2 = nn.Linear(256, 64)
-        self.fc3 = nn.Linear(64, 8)
-        self.fc4 = nn.Linear(8, 1)
+        self.fc1 = nn.Linear(3, 1)
 
     def forward(self, x):
         """
         Forward pass through the network, returns log_softmax values
         """
-        x = torch.tanh(self.fc1(x))
-        x = torch.tanh(self.fc2(x))
-        x = torch.tanh(self.fc3(x))
-        x = torch.tanh(self.fc4(x))
+        x = torch.relu(self.fc1(x))
         return x
 
 
-input_games = []
+input_moves_available = []
+input_threats = []
 input_times = []
-input_moves = []
-#input_scores = []
 
 
 def split(word):
@@ -46,92 +42,76 @@ with open("test_games.pgn") as pgn:
     while game:
         try:
             board = game.board()
-            temp_games = []
-            temp_moves = []
+            temp_moves_available = []
+            temp_threats = []
             temp_times = []
-            #temp_scores= []
 
             for move in game.mainline_moves():
-                #try:
-                #    info = engine.analyse(board, chess.engine.Limit(time=0.1))
-                #    temp_scores.append(info['score'].relative.cp)
-                #except:
-                #    pass
-                temp_char_list = split(board.__str__())
-                temp_list = []
-                init_board = np.zeros((24, 8, 8))
-                row, col = 0, 0
-                for char in temp_char_list:
-                    if char == ' ' or char == '.':
-                        if(char == '.'):
-                            col+=1
-                    elif char == '\n':
-                        row += 1
-                        col = 0
-                        continue
-                    else:
-                        init_board[pieces_dict[char]][row][col] = 1
-                        col += 1
-                        # temp_list.append(pieces_dict[char])
-                temp_games.append(init_board)
-                temp_moves.append(move.__str__())
+                """
+                info = engine.analyse(board, chess.engine.Limit(time=1), root_moves=[move])
+                t = str(info["score"])
+                if t.startswith('#'):
+                    print(str(move), " eval = mate in ", t)
+                else:
+                    print(str(move), " eval = ", round(int(t) / 100., 2))
+                continue
+                """
+                temp_moves_available.append(board.legal_moves.count())
+
+                sum = 0
+                for i in range(8):
+                    for j in range(8):
+                        color = board.color_at(chess.square(j, i))
+                        if color == True:
+                            sum += len(board.attackers(0, chess.square(j, i)))
+                        elif color == False:
+                            sum += len(board.attackers(1, chess.square(j, i)))
+
+                temp_threats.append(sum)
+
                 board.push(move)
 
             for node in game.mainline():
                 if node.next() and node.next().next():
                     temp_times.append(node.clock() - node.next().next().clock())
 
-            for i in range(1, len(temp_games)):
-                for j in range(12):
-                    temp_games[i-1][j+12] = temp_games[i][j]
-            temp_games = temp_games[:-2]
-            temp_moves = temp_moves[:-2]
-            input_moves += temp_moves
-            input_games += temp_games
+            temp_moves_available = temp_moves_available[:-2]
+            temp_threats = temp_threats[:-2]
+            input_threats += temp_threats
+            input_moves_available += temp_moves_available
             input_times += temp_times
-            #input_scores += temp_scores
         except:
             pass
         game = chess.pgn.read_game(pgn)
-print(len(input_moves))
-print(len(input_games))
+print(len(input_threats))
+print(len(input_moves_available))
 print(len(input_times))
-#print(len(input_scores))
 
-input_moves = input_moves[:-48]
-input_games = input_games[:-48]
+input_threats = input_threats[:-48]
+input_moves_available = input_moves_available[:-48]
 input_times = input_times[:-48]
-"""
-min_game = min(input_games)
-max_game = max(input_games)
-min_time = min(input_times)
-max_time = max(input_times)
 
-global_min=999999
-global_max=0
-for i in range(len(input_games)):
-    min_game = min(min(input_games[i]))
-    max_game = max(max(input_games[i]))
-    if min_game<global_min:
-        global_min=min_game
-    if max_game>global_max:
-        global_max=max_game
+npArray1 = np.array(input_threats)
+npArray2 = np.array(input_moves_available)
+npArray3 = np.vstack((npArray1, npArray2)).transpose()
+npArray3 = np.insert(npArray3, 0, values=1, axis=1) # Insert values before column 3
+npArray3 = npArray3.astype(float)
 
-for i in range(len(input_games)):
-    for j in range(i):
+for i in range(1, npArray3.shape[1]):
+    column = npArray3[:, i]
+    maxC = np.amax(column)
+    minC = np.amin(column)
+    if maxC > minC:
+        npArray3[:, i] =((column - minC) / (maxC - minC)) * (1 - (0)) + (0)
 
-for i in range(len(input_times)):
-    v = input_times[i]   # foo[:, -1] for the last column
-    input_times[i] = (v - min_time) / (max_time - min_time)
-"""
-npArray = np.array(input_games)
-tensor_games= np.reshape(npArray, (11200,1536))
-tensor_games = torch.Tensor(tensor_games)
+#shuffler = np.random.permutation(npArray3.shape[0])
+#npArray3 = npArray3[shuffler]
+#input_times = np.array(input_times)[shuffler]
+
+tensor_games = torch.Tensor(npArray3)
 # tensor_moves = torch.Tensor(input_moves)
 tensor_times = torch.Tensor(input_times)
 
-# transforms.Normalize(0, 1)(tensor_times)
-# transforms.Normalize(0, 1)(tensor_games)
 
 my_dataset = data.TensorDataset(tensor_games, tensor_times)
 my_dataloader = data.DataLoader(my_dataset)
@@ -140,8 +120,8 @@ model = NeuralNetwork()
 training_data, validation_data = data.random_split(my_dataset, [9024, 2176])
 train_loader = data.DataLoader(training_data, batch_size=64, shuffle=True)
 val_loader = data.DataLoader(validation_data, batch_size=64, shuffle=False)
-learning_rate = 0.00001
-epochs = 50
+learning_rate = 0.0001
+epochs = 200
 optimizer = optim.Adam(model.parameters(), learning_rate)
 criterion = nn.MSELoss()
 
@@ -185,7 +165,7 @@ def train_model(model, optimizer, criterion, epochs, train_loader, val_loader):
 
 train_losses, val_losses = train_model(model, optimizer, criterion, epochs, train_loader, val_loader)
 list = []
-for i in range(50):
+for i in range(200):
     list.append(i)
 plt.plot(list, train_losses)
 plt.plot(list, val_losses)
