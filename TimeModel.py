49d5e68ee021c8803c4a8021b0d4b8596bc295d2
Netanel Tamir <tamir.netanel@gmail.com>
2021-03-09 21:57:54 +0200

    Worked on improving the first model and parsing features platform

--- TimeModel.py
+++ TimeModel.py
@@ -1,14 +1,9 @@
-import chess.pgn
 import numpy as np
-from matplotlib import transforms
 from torch import nn, optim
 from torch.utils import data
 import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
-from chess import engine
-from timeit import default_timer as timer
-from datetime import timedelta
 
 
 class NeuralNetwork(nn.Module):
@@ -17,23 +12,41 @@ class NeuralNetwork(nn.Module):
         Declare layers for the model
         """
         super().__init__()
-        self.fc1 = nn.Linear(6, 128)
-        self.fc2 = nn.Linear(128, 32)
-        self.fc3 = nn.Linear(32, 1)
-        self.norm1 = nn.BatchNorm1d(128)
-        self.norm2 = nn.BatchNorm1d(32)
+        self.fc1 = nn.Linear(6, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, 64)
+        self.fc5 = nn.Linear(64, 32)
+        self.fc6 = nn.Linear(32, 1)
+        self.norm1 = nn.BatchNorm1d(512)
+        self.norm2 = nn.BatchNorm1d(256)
+        self.norm3 = nn.BatchNorm1d(128)
+        self.norm4 = nn.BatchNorm1d(64)
+        self.norm5 = nn.BatchNorm1d(32)
         self.dropout = nn.Dropout(0.2)
 
     def forward(self, x):
         x = self.fc1(x)
         x = self.norm1(x)
-        x = F.tanh(x)
+        x = F.relu(x)
         x = self.dropout(x)
         x = self.fc2(x)
         x = self.norm2(x)
-        x = F.tanh(x)
+        x = F.relu(x)
         x = self.dropout(x)
         x = self.fc3(x)
+        x = self.norm3(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+        x = self.fc4(x)
+        x = self.norm4(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+        x = self.fc5(x)
+        x = self.norm5(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+        x = self.fc6(x)
         return x
 
 
@@ -56,49 +69,40 @@ def nnTrain(threats_path, moves_available_path, clock_path, taken_path, move_num
     print(len(input_materials))
     print(len(input_times))
 
-    input_threats = input_threats[:-3]
-    input_moves_available = input_moves_available[:-3]
-    input_clock = input_clock[:-3]
-    input_times = input_times[:-3]
-    input_move_num = input_move_num[:-3]
-    input_materials = input_materials[:-3]
-    input_taken = input_taken[:-3]
-
     threat_array = np.array(input_threats)
     moves_available_array = np.array(input_moves_available)
     clock_array = np.array(input_clock)
     move_num_array = np.array(input_move_num)
     taken_array = np.array(input_taken)
-    input_materials = np.array(input_materials)
+    input_materials_array = np.array(input_materials)
     complete_array = np.vstack(
-        (threat_array, moves_available_array, clock_array, taken_array, move_num_array, input_materials)).transpose()
-    # complete_array = np.insert(complete_array, 0, values=1, axis=1) # Insert values before column 1
+        (threat_array, moves_available_array, clock_array, taken_array, move_num_array,
+         input_materials_array)).transpose()
     complete_array = complete_array.astype(float)
 
-    # for i in range(0, complete_array.shape[1]):
-    #     column = complete_array[:, i]
-    #     maxC = np.amax(column)
-    #     minC = np.amin(column)
-    #     if maxC > minC:
-    #         complete_array[:, i] = ((column - minC) / (maxC - minC)) * (1 - (0)) + (0)
+    for i in range(0, complete_array.shape[1]):
+        column = complete_array[:, i]
+        maxC = np.amax(column)
+        minC = np.amin(column)
+        if maxC > minC:
+            complete_array[:, i] = ((column - minC) / (maxC - minC)) * (1 - (-1)) + (-1)
 
-    # shuffler = np.random.permutation(complete_array.shape[0])
-    # complete_array = complete_array[shuffler]
-    # input_times = np.array(input_times)[shuffler]
+    shuffler = np.random.permutation(complete_array.shape[0])
+    complete_array = complete_array[shuffler]
+    input_times = np.array(input_times)[shuffler]
 
     tensor_games = torch.Tensor(complete_array)
     tensor_times = torch.Tensor(input_times)
-    # for i in range(134336):
-    #     if tensor_times[i]>30:
-    #         tensor_times[i]=30
-    my_dataset = data.TensorDataset(tensor_games, tensor_times)
 
+    my_dataset = data.TensorDataset(tensor_games, tensor_times)
+    train_size = int(0.85 * len(my_dataset))
+    validate_size = len(my_dataset) - int(0.85 * len(my_dataset))
     model = NeuralNetwork()
-    training_data, validation_data = data.random_split(my_dataset, [307200, 80960])
+    training_data, validation_data = data.random_split(my_dataset, [train_size, validate_size])
     train_loader = data.DataLoader(training_data, batch_size=64, shuffle=True)
     val_loader = data.DataLoader(validation_data, batch_size=64, shuffle=False)
     learning_rate = 0.001
-    epochs = 10
+    epochs = 100
     optimizer = optim.Adam(model.parameters(), learning_rate)
     criterion = nn.MSELoss()
 
@@ -118,25 +122,26 @@ def nnTrain(threats_path, moves_available_path, clock_path, taken_path, move_num
                 model.train()
                 optimizer.zero_grad()
                 output = model.forward(images)
-                new_labels = np.reshape(labels, (64, 1))
-                loss = criterion(output, new_labels)
+                labels = torch.unsqueeze(labels, 1)
+                labels = labels.type(torch.FloatTensor)
+                loss = criterion(output, labels)
                 loss.backward()
                 optimizer.step()
 
                 running_loss += loss.item()
             else:
-                val_loss = 0
                 # 6.2 Evalaute model on validation at the end of each epoch.
                 with torch.no_grad():
                     for images, labels in val_loader:
                         output = model.forward(images)
-                        new_labels = np.reshape(labels, (64, 1))
-                        # if e == 2:
-                        #     for image,i,j in zip (images,output,new_labels):
-                        #         print(str(image)+" "+str(i[0])+" "+str(j[0]))
-                        avg_tensor = torch.tensor(np.full((64, 1), avg))
-                        avg_loss = criterion(avg_tensor, new_labels)
-                        val_loss = criterion(output, new_labels)
+                        labels = torch.unsqueeze(labels, 1)
+                        labels = labels.type(torch.FloatTensor)
+                        if e == epochs - 1:
+                            for image, i, j in zip(images, output, labels):
+                                print(str(int(i)) + " " + str(int(j)))
+                        avg_tensor = torch.full_like(labels, avg)
+                        avg_loss = criterion(avg_tensor, labels)
+                        val_loss = criterion(output, labels)
                         running_val_loss += val_loss.item()
                         running_avg_loss += avg_loss.item()
 
@@ -148,17 +153,20 @@ def nnTrain(threats_path, moves_available_path, clock_path, taken_path, move_num
             print("Epoch: {}/{}.. ".format(e + 1, epochs),
                   "Training Loss: {:.3f}.. ".format(running_loss / len(train_loader)),
                   "Validation Loss: {:.3f}.. ".format(running_val_loss / len(val_loader)),
-                  "Average Loss: {:.3f}.. ".format(running_avg_loss / len(val_loader)))
+                  "Average Loss: {:.3f}.. ".format(running_avg_loss / len(val_loader)
+                                                   ))
 
-        return train_losses, val_losses
+        return train_losses, val_losses, avg_losses
 
-    train_losses, val_losses = train_model(model, optimizer, criterion, epochs, train_loader, val_loader)
+    train_losses, val_losses, avg_losses = train_model(model, optimizer, criterion, epochs, train_loader, val_loader)
     list = []
-    for i in range(10):
+    for i in range(epochs):
         list.append(i)
     plt.plot(list, train_losses)
     plt.plot(list, val_losses)
+    plt.plot(list, avg_losses)
     plt.show()
 
 
-nnTrain('threats.txt', 'available_moves.txt', 'clock.txt', 'taken.txt', 'count_moves.txt', 'materials.txt', 'times.txt')
+nnTrain('masters_threats.txt', 'masters_available_moves.txt', 'masters_clock.txt', 'masters_taken.txt',
+        'masters_moves.txt', 'masters_materials.txt', 'masters_times.txt')
