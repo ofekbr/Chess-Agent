7b8ad25775ec1584caa73426d3f100c1bb122fd5
Ofek <barkai.ofek@gmail.com>
2021-03-15 19:29:43 +0200

    new game files

--- gamecutter.py
+++ gamecutter.py
@@ -2,8 +2,8 @@ from chess.pgn import read_game
 from timeit import default_timer as timer
 
 
-full_pgn = open("lichess_db_standard_rated_2017-04.pgn")
-new_pgn = open("1800sec_400_2000.pgn", "w")
+full_pgn = open("lichess_db_standard_rated_2021-01.pgn")
+new_pgn = open("atleast2400sec_400diff_above2000.pgn", "w")
 
 game = read_game(full_pgn)
 skipped, i, j = 0, 0, 0
@@ -15,14 +15,19 @@ while game is not None:
     time_control = headers["TimeControl"]
     if time_control == "-":
         game = read_game(full_pgn)
+        skipped += 1
         continue
     seconds, addition = time_control.split("+")
     seconds = int(seconds)
-    if w_elo > 1800 and b_elo > 1800 and abs(w_elo-b_elo) < 400 and int(seconds) >= 1800 and addition == '0':
+    if w_elo > 2000 and b_elo > 2000 and abs(w_elo-b_elo) < 400 and int(seconds) >= 1800 and addition == '0':
         i += 1
         print(f"{i} / {j} skipped-{skipped}")
         new_pgn.write(game.__str__())
         new_pgn.write("\n\n")
+        if i > 3000:
+            break
+    if j % 2000 == 0:
+        print(f"{i} / {j} skipped-{skipped}")
     game = read_game(full_pgn)
 
 full_pgn.close()
